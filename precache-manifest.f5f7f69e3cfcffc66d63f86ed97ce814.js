13bb87ea9e4e79cbdb89471622258eb9ee2d969d
amarendra <amarendra.yadav@shoppertrak.com>
2020-06-03 23:32:40 +0530

    Updates

---
+++ precache-manifest.f5f7f69e3cfcffc66d63f86ed97ce814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea1f5707bd6fee0eb828743fbaa1d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1fa037eb0845625f594",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "75dcd754653b8dd2bb00",
+    "url": "/static/js/2.5d1520d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d1520d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fa037eb0845625f594",
+    "url": "/static/js/main.642a3ebc.chunk.js"
+  },
+  {
+    "revision": "7129a994307f4aa325e4",
+    "url": "/static/js/runtime-main.c8f32266.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
